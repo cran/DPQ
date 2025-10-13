d476d596a985b7bafb7929a2594a6d728901271a
Martin Maechler <maechler@stat.math.ethz.ch>
2025-10-13 16:20:12 +0000

    version 0.6-1

--- R/beta-fns.R
+++ R/beta-fns.R
@@ -782,8 +782,63 @@ gam1d <- function(a, warnIf=TRUE, verbose=FALSE) { # ../src/bpser.c
     .Call(C_R_gam1, a, warnIf, verbose)
 }
 
+gam1 <- function(a, useTOMS = is.numeric(a), warnIf=TRUE, verbose=FALSE) {
+## // == 1/gamma(a+1) - 1
+    if(!length(a)) return(a)
+    if(useTOMS) ##  --- double precision version -- from toms708.c --------
+      ## /*     ------------------------------------------------------------------ */
+      ## /*     COMPUTATION OF 1/GAMMA(A+1) - 1  FOR -0.5 <= A <= 1.5 */
+      ## /*     ------------------------------------------------------------------ */
+      gam1d(a, warnIf=warnIf, verbose=verbose)
+    else if(inherits(a, "mpfr")) { ## >>>> identical  to DPQmpfr::gam1M()  <<<<<
+        pr_a <- Rmpfr::getPrec(a)
+        roundM <- Rmpfr::roundMpfr
+        ## use larger precision
+        bits <- ceiling(-log2(abs(a)))
+        bits[a == 0] <- 0
+        usePr <- pr_a + 2L + pmax(as.integer(bits), 0L)
+        ## and "round back"
+        roundM(1/gamma(roundM(a, usePr) + 1) - 1, pr_a)
+        ##     ^^^^^^^^       ^       ^^^^^^^^^^
+    } else {
+        warning("not numeric, nor \"mpfr\" -- using direct, possibly inaccurate formula")
+        1/gamma(a + 1) - 1
+    }
+} ## {gam1}
+
+
 ## _yet_ another lgamma1p() , name and C code from TOMS 708 (R/src/nmath/toms708.c
-gamln1 <- function(a, warnIf=TRUE) .Call(C_R_gamln1, a, warnIf) # --> ../src/bpser.c
+gamln1. <- function(a, warnIf=TRUE) .Call(C_R_gamln1, a, warnIf) # --> ../src/bpser.c
+
+gamln1 <- function(a, useTOMS = is.numeric(a), warnIf=TRUE) {
+  ##  == lgamma(a+1)    bpser() used it only for 0 <= a0 < 1
+  ## * ----------------------------------------------------------------------- */
+  ## *     EVALUATION OF LN(GAMMA(1 + A)) FOR -0.2 <= A <= 1.25 */
+  ## * ----------------------------------------------------------------------- */
+
+  ## FIXME: also SHOULD HAVE  lgamma1p()
+  ##       vvvvvvvvvvvvvvv    ---------- with Maple derived formulas,
+  ## from  lgamma1p_series() up to 15 terms !! --- should (?) use also with MPFR ?
+  if(!length(a)) return(a)
+  if(useTOMS) #  --- double precision version -- from toms708.c --------
+    gamln1.(a, warnIf=warnIf)
+  else if(inherits(a, "mpfr")) { ## FIXME -- for really small 'a', have  DPQ :: lgamma1p_series() *working* with "mpfr"
+    pr_a <- Rmpfr::getPrec(a)
+    roundM <- Rmpfr::roundMpfr
+    ## use larger precision
+    bits <- ceiling(-log2(abs(a)))
+    bits[a == 0] <- 0
+    usePr <- pr_a + 2L + pmax(as.integer(bits), 0L)
+    ## and "round back"
+    roundM(lgamma(roundM(a, usePr) + 1), pr_a)
+    ##     ^^^^^^^       ^       ^^^^^^
+  } else {
+    warning("not numeric, nor \"mpfr\" -- using direct, possibly inaccurate formula")
+    ## TODO?  switch between    log(a) + lgamma(a)   and the following
+    lgamma(a + 1)
+  }
+}
+
 
 
 ##  ../src/gamma_inc_T1006.c -- TOMS 1006 "fast and accurate eval of a generalized incomplete gamma function"
@@ -1235,6 +1290,110 @@ qbeta.R  <-  function(alpha, p, q,
     ## L_converged:
     if(verbose) cat(" ", o.it,"outer iterations\n")
     if (swap.tail) 1 - xinbta else xinbta
+}
+
+###---------------- Abramowitz & Stegun, 26.5 Beta Distribution  --- p.945 -- Approximations
+
+###------------- I am particularly interested for  log.p=TRUE  "very outer tail" where pbeta(*, log.p=FALSE) == 0
+
+### NB:  {DPQmpfr}  pbetaD94() *and* pbeta_ser()
+
+## via chi^2 -- A. & S. p.945, eq. 26.5.20 --------------------------------------
+## fix formulas without checking -- for exploration
+.pbeta.eq20 <- function(q, a,b, lower.tail = TRUE, log.p = FALSE,
+                        ._1_q = .5 - q + .5) # 1-q "carefully"
+{
+    ch2 <- ((a+b-1) * (3-q) - (b-1)) * ._1_q # (should recycle fine)
+    pchisq(ch2, df = 2*b, lower.tail=!lower.tail, log.p=log.p)
+}
+## swapped tail version
+.pbetaSeq20 <- function(q, a,b, lower.tail = TRUE, log.p = FALSE)
+  .pbeta.eq20(.5 - q + .5, b, a, lower.tail = !lower.tail, log.p=log.p, ._1_q = q)
+
+pbetaAS_eq20 <- function(q, shape1, shape2, lower.tail = TRUE, log.p = FALSE, warnBad = TRUE)
+{
+    a <- shape1
+    b <- shape2
+    ab1 <- a + b - 1
+    ._1_q <- .5 - q + .5 # 1-q "carefully"
+    ab1x <- ab1 * ._1_q # (a+b-1)(1-q)  using {a,b, q} ==> properly recycled
+    n <- length(r <- ab1x)
+    if(!n) return(r) # of length 0
+    if(n > length(q)) ._1_q <- rep_len(._1_q, n)
+    if(n > length(a)) a <- rep_len(a, n)
+    if(n > length(b)) b <- rep_len(b, n)
+    ok <- (ab1x <= 0.8) # "necessary" by A & S {== iff (26.5.21) is not good }
+    swap <- !ok & (ab1*q <= 0.8) # swapping will be ok
+    bad <- !ok & !swap
+    if(warnBad && any(bad))
+        warning("potentially have ", sum(bad)," 'bad'ly approximated pbeta values")
+    if(any(swap))
+        r[swap] <- pbetaAS_eq20(._1_q[swap], b[swap], a[swap],  # warn*: shdnt happen
+                                lower.tail = !lower.tail, log.p=log.p, warnBad=TRUE)
+    ## if(any(ok)) .. rather deal with "bad" as well:
+    if(any(ok <- !swap)) {
+        b <- b[ok]
+        ch2 <- (ab1x*(3-q))[ok] - ._1_q[ok]*(b-1)
+        nu <- 2*b
+        r[ok] <- pchisq(ch2, df=nu, lower.tail=!lower.tail, log.p=log.p)
+    }
+    r
+}
+
+## via normal -- A. & S. p.945, eq. 26.5.21,
+## fix formulas without checking -- for exploration
+.pbeta.eq21 <- function(q, a,b, lower.tail = TRUE, log.p = FALSE,
+                        ._1_q = .5 - q + .5) # 1-q "carefully"
+{
+    w1 <- (b *    q )^(1/3)
+    w2 <- (a * ._1_q)^(1/3)
+    pnorm(3*(w1-w1/(9*b) - (w2-w2/(9*a))) / sqrt(w1^2/b + w2^2/a),
+          lower.tail=lower.tail, log.p=log.p)
+}
+## swapped tail version
+.pbetaSeq21 <- function(q, a,b, lower.tail = TRUE, log.p = FALSE)
+  .pbeta.eq21(.5 - q + .5, b, a, lower.tail = !lower.tail, log.p=log.p, ._1_q = q)
+
+
+pbetaAS_eq21 <- function(q, shape1, shape2, lower.tail = TRUE, log.p = FALSE, warnBad = TRUE) {
+    a <- shape1
+    b <- shape2
+    ab1 <- a + b - 1
+    ._1_q <- .5 - q + .5 # 1-q "carefully"
+    ab1x <- ab1 * ._1_q # (a+b-1)(1-q)  using {a,b, q} ==> properly recycled
+    n <- length(r <- ab1x)
+    if(!n) return(r) # of length 0
+    if(n > length(q)) ._1_q <- rep_len(._1_q, n)
+    if(n > length(a)) a <- rep_len(a, n)
+    if(n > length(b)) b <- rep_len(b, n)
+    ok <- (ab1x >= 0.8) # "necessary" by A & S {== iff (26.5.20) is not good }
+    swap <- !ok & (ab1*q >= 0.8)
+    bad <- !ok & !swap
+    if(warnBad && any(bad))
+        warning("potentially have ", sum(bad)," 'bad'ly approximated pbeta values")
+    if(any(swap))
+        r[swap] <- pbetaAS_eq21(._1_q[swap], b[swap], a[swap],  # warn*: shdnt happen
+                                lower.tail = !lower.tail, log.p=log.p, warnBad=TRUE)
+    ## if(any(ok)) .. rather deal with "bad" as well:
+    if(any(ok <- !swap)) {
+        a <- a[ok]
+        b <- b[ok]
+        q <- rep_len(q, n)[ok]
+        w1 <- (b *     q    )^(1/3)
+        w2 <- (a * ._1_q[ok])^(1/3)
+        r[ok] <- pnorm(3*(w1-w1/(9*b) - (w2-w2/(9*a))) / sqrt(w1^2/b + w2^2/a),
+                       lower.tail=lower.tail, log.p=log.p)
+    }
+    r
+}
+
+pbetaNorm2 <- function(q, shape1, shape2, lower.tail = TRUE, log.p = FALSE) {
+  a <- shape1
+  b <- shape2
+  apb <- a+b
+  mu <- a/apb
+  sd <- sqrt(a*b)/apb/sqrt(apb+1)
+  pnorm(q, mu, sd, lower.tail=lower.tail, log.p=log.p)
 }
 
 ## Local Variables:
--- tests/chisq-nonc-ex.R
+++ tests/chisq-nonc-ex.R
@@ -50,18 +50,18 @@ showProc.time()
 ### densities alone :
 ## ===> shows Normal limit (for lambda -> Inf;  true also for nu -> Inf)
 nu <- 12
-nS <- length(ncSet <- if(doExtras) 10^(0:9) else 10^(0:6))
+nS <- length(ncSet <- if(doExtras) 10^seq(1, 9, by=1/2) else 10^(1:6))
 np <- if(doExtras) 201 else 64
 cpUse <- numeric(nS); names(cpUse) <- formatC(ncSet)
-mult.fig(nS, main = paste("non-central chisq(*, df=",nu,
-                          ") and normal approx"))$old.par -> op
+mult.fig(nS, marP = -0.8,
+         main = paste0("non-central chisq(*, df=",nu,") and normal approx"))$old.par -> op
 for(NC in ncSet) {
     m <- NC + nu
     s <- sqrt(2*(nu + 2*NC))
-    x <- seq(from= m - 3*s, to= m + 3*s, length = np)
+    x <- seq(from= m - 3*s, to= m + 3*s, length.out = np)
     cpUse[formatC(NC)] <- system.time(y <- dchisq(x, df=nu, ncp=NC))[1]
     plot(x, y, ylim=c(0,max(y)),type = "l", ylab='f(x)', main=paste("ncp =",NC))
-    lines(x, dnorm(x,m=m,s=s), col = 'blue')
+    lines(x, dnorm(x, mean=m, sd=s), col = 'blue')
 }
 par(op)# resetting mult.fig()
 showProc.time()
@@ -69,8 +69,8 @@ showProc.time()
 cbind(ncSet, cpUse, "c/ncp"= cpUse / ncSet)
 ## fails on Win 32b: "need finite 'ylim' values" :
 try(plot(cpUse ~ ncSet, log = "xy", type = 'b', col = 2))
-if(doExtras) try(# fails occasionally (too many zeros)
-  print(summary(lmll <- lm(log(cpUse) ~ log(ncSet), subset = ncSet >= 1e4)))
+if(doExtras) try(# fails occasionally (too many zeros -> log(0) = -Inf)
+  print(summary(lmll <- lm(log(cpUse) ~ log(ncSet), subset = cpUse > 0)))
 )
 ## Coefficients:
 ##              Estimate Std. Error t value Pr(>|t|)
@@ -92,7 +92,7 @@ if(doExtras) try(# fails occasionally (too many zeros)
 ncp <- 16
 nS <- length(dfSet <- 10^(if(doExtras) 0:11 else 0:8))
 cpUse <- numeric(nS); names(cpUse) <- formatC(dfSet)
-oPar <- mult.fig(nS, main = "non-central chisq(ncp = 16) and normal approx")$old.par
+oPar <- mult.fig(nS, marP = -0.75, main = "non-central chisq(ncp = 16) and normal approx")$old.par
 for(DF in dfSet) {
     m <- DF + ncp
     s <- sqrt(2*(DF + 2*ncp))
@@ -111,9 +111,9 @@ showProc.time()
 ## R
 curve(dnoncentchisq(x, df=3, ncp=0), 0, 10)
 curve(dchisq       (x, df=3),        0, 10, add=TRUE, col='purple')
-## ok
+## *no* visible difference -- ok
 curve(dnoncentchisq(x, df=3, ncp=1), 0, 10)
-curve(dchisq       (x, df=3, ncp=1), 0, 10, add=TRUE, col='purple') #ditto
+curve(dchisq       (x, df=3, ncp=1), 0, 10, add=TRUE, col='purple') # ditto
 
 x  <- seq(0, 10, length=101)
 del <- c(0:4,10,40)
@@ -221,9 +221,10 @@ p.dnchiB <- function(df, ncp, log=FALSE, from=0, to = 2*ncp, p.log="", n = if(do
 }
 
 ## simple check
-stopifnot(all.equal(dchisq(1:30, df=3, ncp=1:30),
-                    dnchisqBessel(1:30, df = 3, ncp = 1:30),
-                    tol = 1e-13)) ## tol=0 --> "Mean rel.diff.: 2.3378e-14"
+dchR  <- dchisq       (1:30, df = 3, ncp = 1:30)
+dchBI <- dnchisqBessel(1:30, df = 3, ncp = 1:30)
+          all.equal(dchR, dchBI, tol = 0) # "Mean rel.diff.: 2.3378e-14"
+stopifnot(all.equal(dchR, dchBI, tolerance = 1e-13))
 
 p.dnchiB(df=1.2, ncp=500,, 200, 800)
 p.dnchiB(df=1.2, ncp=500, log=TRUE)# differ in tail
@@ -239,14 +240,13 @@ p.dnchiB(df=35, ncp=400, log=TRUE, 0,1500)    # the same
 p.dnchiB(df= 3, ncp=600, log=TRUE, 0,2500)    # the same
 p.dnchiB(df= 3, ncp=800, log=TRUE, 0,3500)    # for large x --> NaN in besselI
 
-## However, large  ncp  -- gives overflow in besselI():
-dnchisqBessel(8000, df=20, ncp=5000) ## NaN -- no longer: now 1.3197e-78
+## Originally, large  ncp  gave overflow in besselI():
+dnchisqBessel(8000, df=20, ncp=5000) # 1.3197e-78
+       dchisq(8000, df=20, ncp=5000) # 6.8563e-79
 
-## Hmm, I'm slightly confused that the cutoff seems at 1500 [ < 1e4 !]
+## This is all fine now :
 x <- if(doExtras) 1000:1600 else seq(1000, 1600, by = 5)
-plot (x, besselI(x, 9, TRUE), type="l")
-## Warning message:
-## In besselI(x, nu, 1 + as.logical(expon.scaled)) : NaNs produced
+plot (x, besselI(x, 9,   TRUE), type="l")
 lines(x, besselI(x, 1.2, TRUE), col=2)
 lines(x, besselI(x, 1.0, TRUE), col=2)
 lines(x, besselI(x, 0.1, TRUE), col=2)
@@ -265,8 +265,8 @@ lines(x, besselI(x, 7.2, TRUE), col="blue")
 lines(x, besselI(x, 8.2, TRUE), col="blue")
 ##--> Need asymptotic for besselI(x, nu) with a term that depends on nu
 
-##--> ...bessel-large-x.R  and better  ~/R/Pkgs/Bessel/
-##       ~~~~~~~~~~~~~~~~~~~[April 2008]  ================
+##--> ~/R/MM/NUMERICS/Bessel/bessel-large-x.R  and better  ~/R/Pkgs/Bessel/
+##    ~~~~~~~~~~~~~~~~~~~[April 2008]  ================
 
 showProc.time()
 
@@ -656,10 +656,10 @@ par(op)
 ## ----------- i >= lambda  : need better approx
 plotrq <- function(lambda, i = 1:(3*round(lambda))) {
     lab <- as.expression(substitute(lambda==la, list(la=lambda)))
-    plot(i, r_pois(i,lam=lambda) / (lambda/i),
+    plot(i, r_pois(i,lambda=lambda) / (lambda/i),
          type='b', cex=.4, col=2)
     abline(v=lambda, col='gray', lty=2)
-    axis(3, at = lambda, label = lab)
+    axis(3, at = lambda, labels = lab)
 }
 plotrq(10)
 
@@ -696,14 +696,19 @@ par(op)
 
 ### Now back to the original problem:
 ### Using ss() terms and see where they are maximal, etc.
-(pR <-          pnchisq (1.2,df=1,ncp=3, verbose=FALSE))# iter = 12, now 13
+(pR <-           pnchisq   (1.2,df=1,ncp=3, verbose=FALSE))# iter = 12, now 13
 all.equal(c(pR), pnchisq_ss(1.2,df=1,ncp=3), tol=0)# 2.19e-12, now 9.61e-14,
 ## 6.4e-16 on Win 32b !
 
-(pR <-          pnchisq (1.2,df=1,ncp=30, verbose=FALSE))# iter = 12, now 16
-all.equal(pR, pnchisq_ss(1.2,df=1,ncp=30), tol= 2e-13)
+(pR  <- pnchisq   (1.2, df=1, ncp=30, verbose=FALSE))# iter = 12, now 16
+(pss <- pnchisq_ss(1.2, df=1, ncp=30))
+all.equal(pR, pss, tol = 0)
+## Now you can also pass an ss() result to pnchisq_ss():
+str(ss1.2 <- ss(1.2, df=1, ncp=30))
+stopifnot(identical(pss,
+                    pss. <- pnchisq_ss(1.2, df=1, ncp=30, ssr = ss1.2)))
 ## was  2.616 e-8 (thanks to 'reltol'!)
-(pR <-          pnchisq (1.2,df=1,ncp=30, verbose=FALSE,reltol=3e-16))# 19 it.
+(pR <-        pnchisq   (1.2,df=1,ncp=30, verbose=FALSE, reltol=3e-16))# 19 it.
 all.equal(pR, pnchisq_ss(1.2,df=1,ncp=30), tol= 2e-16)
 
 str(sss <- ss(1.2,df=1,ncp=30))# s[1:161], max = 3
